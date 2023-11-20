623ecc5ddf939839f5c5b1c4f1f844c8d30f0536
Ramanand Tiwari <112823601+Ramanand1101@users.noreply.github.com>
2023-11-20 23:24:45 +0530

    backend app fully created

---
+++ router/userRouter.js
@@ -0,0 +1,61 @@
+// routes/userRoutes.js
+const express = require('express');
+const router = express.Router();
+const User = require('../model/userModel');
+
+router.post('/users', async (req, res) => {
+  try {
+    const user = new User(req.body);
+    await user.save();
+    res.status(201).json(user);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+router.get('/users', async (req, res) => {
+  try {
+    const users = await User.find();
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+router.get('/users/:id', async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+router.delete('/users/:id', async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id);
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+    res.status(200).json({ message: 'User deleted successfully' });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+router.put('/users/:id', async (req, res) => {
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,32 @@
+// server.js
+const express = require('express');
+const bodyParser = require('body-parser');
+const connectDB = require("./confiq/db");
+const userRoutes = require('./router/userRouter');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Connect to MongoDB
+
+
+app.use(bodyParser.json());
+
+// Routes
+app.use('/api', userRoutes);
+
+app.listen(PORT, async () => {
+  try {
+    await connectDB
+    console.log("Database connected Successfully")
+  }
+  catch (err) {
+    console.error(err.message)
+    process.exit(1)
+  }
+  console.log(`Server is running on port ${PORT}`);
+});
+
+
+
+
